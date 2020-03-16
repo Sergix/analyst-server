d1110cfae57d1a66f3755d7993665602a9cccda6
Wesley Ford <mangofordw@gmail.com>
2020-03-15 23:52:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+# Main Backend Code for Analyst 
+
+#Import Flask
+from flask import Flask
+
+#Import urllib as stockQuery
 Import urllib as stockQuery
 
+#Flask instance
+#Which May need to be reconfigured 
+app = Flask(__name__)
 
 #flaskRequests
