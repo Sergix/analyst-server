479989e4c6210017055e6e8b33a9fb91d5d240f2
Wesley Ford <mangofordw@gmail.com>
2020-03-15 23:17:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-#This is a comment
-
-thisIsAString = "Hello World"
-#This is a string
-
-thisIsANumber = 13
-#This is a number
-
-thisIsAnArray = [1,"hey",2]
-#this IsAn Array
-
-print(thisIsAnArray[1]);
-#Outputs "hey"
-#index: 0,  1  ,2
-#index: 1,"hey",2
+Import urllib as stockQuery
