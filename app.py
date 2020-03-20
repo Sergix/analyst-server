6266564ade21f75e0063bbccd45771c2b2f087d8
Wesley Ford <mangofordw@gmail.com>
2020-03-20 19:55:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-# Main -Brain- Backend Code for Analyst 
-from flask import Flask, request
-import stock_api_handler
+from flask import Flask, render_template, request, abort
+from stock_api_handler import ApiHandler
 
-#create Flask instance
 app = Flask(__name__)
 
-@app.route('/')
-def on_request():
-    #here we will handle all of our request
+
+@app.route("/stockQuery/")
+def client_side_query():
+  #if client requesting data -get- 
+  if request.method == "GET":
+    api = ApiHandler()
+    #our ticker aka stock letter
+    t = request.args['ticker']
+    #our query type 
+    f = request.args['request']
+    #optional query interval
+    i = request.args.get('interval')
+    #return api data
+    return(str(api.autoQuery(t,f,i)))
+  else:
+    #abort bad request
+    abort(400)
+
+if __name__ == "__main__":
+  app.run()
