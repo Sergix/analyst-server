246fd5db43ac69fdf13e882fce242b871eb035eb
Wesley Ford <mangofordw@gmail.com>
2020-03-20 22:33:22 -0400

    Update stock_api_handler.py

--- stock_api_handler.py
+++ stock_api_handler.py
@@ -3,118 +3,118 @@
 # Credits:nóto
 
 #Import request and parse from urllib so we can request from the api
-from urllib import request, parse
+from urllib import request as req
+from urllib import parse
 #Import json to manipulate api data
 import json
 
 
 #create a class to handle our stock api
 class ApiHandler:
-    def __init__(Self):
-        Self.Url = "https://www.alphavantage.co/"
-        Self.apikey = "4WXA1UZXP1MVFLPG"
+    def __init__(self):
+        self.Url = "https://www.alphavantage.co/"
+        self.apikey = "4WXA1UZXP1MVFLPG"
     #END
 
     #this method creates our api queryurls
-    def createQuery(Self, t, r, i=None):
+    def createQuery(self, t, r, i=None):
         # t -ticker- is the stock letters
         # r -response type- is the type of request we are going to be making to the api
         # i is the interval 
         # we will assume that all our parameters correct
-        Self.function = r
-        Self.Stock = t
+        self.function = r
+        self.Stock = t
         if(r == "points"):
             #We need to return stock points from the api for charting on the front end
             if(i == None):
                 i = "5min"
-            Self.queryParam = {
+            self.queryParam = {
                 "function":"TIME_SERIES_DAILY_ADJUSTED",
-                "symbol":Self.Stock,
+                "symbol":self.Stock,
                 "interval": i,
                 "datatype":"json",
-                "apikey":Self.apikey
+                "apikey":self.apikey
                 }
 
-            Self.parseQuery()
+            self.parseQuery()
             
         #END
         elif(r == "stock"):
             #We need to return stock info from the api
-            Self.queryParam = {
+            self.queryParam = {
                 "function":"GLOBAL_QUOTE",
-                "symbol":Self.Stock,
+                "symbol":self.Stock,
                 "datatype":"json",
-                "apikey":Self.apikey
+                "apikey":self.apikey
                 }
-            Self.parseQuery()
+            self.parseQuery()
             
             #END
         elif(r == "search"):
             #We need to search stock letters from the api
-             Self.queryParam = {
+             self.queryParam = {
                 "function":"SYMBOL_SEARCH",
-                "keywords":Self.Stock,
+                "keywords":self.Stock,
                 "datatype":"json",
-                "apikey":Self.apikey
+                "apikey":self.apikey
                 }
              
-             Self.parseQuery()
+             self.parseQuery()
         else :
             raise ValueError('Error: Incorrect Handler Request Method')
             #END
     #END
     
-    def parseQuery(Self):
+    def parseQuery(self):
         #parse the api paramaters
-        Self.queryParam = parse.urlencode(Self.queryParam)
+        self.queryParam = parse.urlencode(self.queryParam)
         #create the api url query string
-        Self.query = Self.Url + "query?"+ Self.queryParam
+        self.query = self.Url + "query?"+ self.queryParam
     #END
         
-    def retrieveQuery(Self):
+    def retrieveQuery(self):
         #query the srtring and the store api's response
-        Self.apiResp = request.urlopen(Self.query)
+        self.apiResp = req.urlopen(self.query)
         #if connection is connected
-        if(Self.apiResp.isclosed() == False):
+        if(self.apiResp.isclosed() == False):
             #Store json Data
-            Self.json = json.load(Self.apiResp)
+            self.json = json.load(self.apiResp)
         else:
             raise ValueError('Error: Api Connection Failed')
         #clean Json and return Data
-        Self.cleanJson(len(Self.json))
-        
-    def cleanJson(Self, a=1):
-        Self.data = {
-                Self.function:{
+        self.cleanJson(len(self.json))   
+    def cleanJson(self, a=1):
+        self.data = {
+                self.function:{
                         #We will update-insert- data here
                     }
              }
         if a == 2:
             #There is meta data let's get ride of it and clean our json
             #loop over json keys
-            for i in Self.json[(list(Self.json)[1])]:
+            for i in self.json[(list(self.json)[1])]:
                 #create final result
-                Self.data[Self.function].update(
+                self.data[self.function].update(
                     {
                         #We will fix this later lol
-                         i:list(((Self.json[(list(Self.json)[1])])[i]).values())
+                         i:list(((self.json[(list(self.json)[1])])[i]).values())
                     })
-            return(Self.data)
-        elif a == 1 and Self.function == "stock":
+            return(self.data)
+        elif a == 1 and self.function == "stock":
             #There is no meta data but, we need to clean our json
             #loop over json keys
-            for i in Self.json[list(Self.json)[0]]:
+            for i in self.json[list(self.json)[0]]:
                 #create final result
-                Self.data[Self.function].update(
+                self.data[self.function].update(
                     {
                         #We will fix this later lol
-                        (str(i)[4:]):(Self.json[(list(Self.json)[0])]).values()
+                        (str(i)[4:]):(self.json[(list(self.json)[0])]).values()
                      })
-        elif a == 1 and Self.function=="search":
+        elif a == 1 and self.function=="search":
              #There is no meta data but, we need to clean our json
             #loop over json keys
-            for i in Self.json[list(Self.json)[0]]:
-                Self.data[Self.function].update(
+            for i in self.json[list(self.json)[0]]:
+                self.data[self.function].update(
                     {
                         #We will fix this later lol
                         (i[(list(i)[0])]):list(i.values())
@@ -124,19 +124,12 @@ class ApiHandler:
             
         #END
     #make life easy :)
-    def autoQuery(Self, t, f, i="5min"):
+    def autoQuery(self, t, f, i="5min"):
         #create our query URL
-        Self.createQuery("AA", "search")
+        self.createQuery(t,f,i)
         #fetch data
-        Self.retrieveQuery()
+        self.retrieveQuery()
         #return data
-        return Self.data
+        return self.data
         #END
     #END
-    
-#operations
-#create instance of class
-stockApi = ApiHandler()
-
-#Thats All Folks 
-#P.s for now
