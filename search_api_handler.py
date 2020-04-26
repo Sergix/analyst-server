74aa4a775df07c570a0827680fcde9eafc8d5873
Wesley Ford <mangofordw@gmail.com>
2020-04-26 16:06:52 -0400

    Update search_api_handler.py

--- search_api_handler.py
+++ search_api_handler.py
@@ -25,11 +25,8 @@ class SearchApi():
         else:
             raise ValueError('Error: Api Connection Failed')
         #return the data cleaned
-        
-        self.json = {
-            self.json
-        }
-        return json.dump(self.json)
+       
+        return self.json
     #END
     def clean_data(self):
         #create a dictionary to store new data 
